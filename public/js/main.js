383e2ad7b6fa7039c57c4624aad172d327d36e1f
walters954 <wwalt010@fiu.edu>
2016-03-22 18:53:51 -0400

    Added log in page
    Added CSS to make log in page work

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,29 @@
 var nickname = 'guest';
-var socket = io('http://nuilabs.org:8082');
+var socket = io('http://localhost:8083');
 
 $(function() {
   socket.on('connect', function() {
+    // If uuid doesn't exist
     if (!(uuid = localStorage.getItem('uuid'))) {
       var randomlyGeneratedUID = Math.random().toString(36).substring(3, 16) + +new Date;
+      // Generate A uuid and store it locally
       localStorage.setItem('uuid', randomlyGeneratedUID);
     }
-
+    //Send our uuid to the 'register'.
     socket.emit('register', uuid);
   });
 
 });
 
+// Emit a 'message' to the server.
 $('form').submit(function() {
-  if (/\S/.test($('#m').val()))
+  if (/\S/.test($('#m').val())) // If it's not whitespace
     socket.emit('message', $('#m').val());
-  $('#m').val('');
+  $('#m').val(''); //Reset the input
   return false;
 });
 
-// receive msg
+// receive 'message' from server
 socket.on('message', function(msg) {
   $('#messages').append($('<li>').text(msg));
 });
@@ -30,4 +33,19 @@ socket.on('nickname', function(msg) {
 });
 socket.on('clear', function() {
   $('#messages').html("");
+});
+
+/*Warren */
+jQuery(document).ready(function() {
+    jQuery('.tabs .tab-links a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+
+        // Show/Hide Tabs
+        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
+    });
 });
