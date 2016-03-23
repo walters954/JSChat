ace1f653d9a141a8d3679309e2833fd515597f95
walters954 <wwalt010@fiu.edu>
2016-03-22 23:48:19 -0400

    Going forwards

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,27 @@
 var nickname = 'guest';
 var socket = io('http://localhost:8082');
 
+
 $(function() {
   socket.on('connect', function() {
-    // If uuid doesn't exist
     if (!(uuid = localStorage.getItem('uuid'))) {
       var randomlyGeneratedUID = Math.random().toString(36).substring(3, 16) + +new Date;
-      // Generate A uuid and store it locally
       localStorage.setItem('uuid', randomlyGeneratedUID);
     }
-    //Send our uuid to the 'register'.
+
     socket.emit('register', uuid);
   });
 
 });
 
-// Emit a 'message' to the server.
 $('form').submit(function() {
-  if (/\S/.test($('#m').val())) // If it's not whitespace
+  if (/\S/.test($('#m').val()))
     socket.emit('message', $('#m').val());
-  $('#m').val(''); //Reset the input
+  $('#m').val('');
   return false;
 });
 
-// receive 'message' from server
+// receive msg
 socket.on('message', function(msg) {
   $('#messages').append($('<li>').text(msg));
 });
